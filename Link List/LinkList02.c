c8129b3f9113cad0f36b3e81938696d1b5747e3a
panditprogrammer <panditprogrammer@gmail.com>
2021-04-12 11:47:37 +0530

    link list delete node show list node

---
+++ Link List/LinkList02.c
@@ -0,0 +1,132 @@
+ //link list
+ //deleting node
+#include <stdio.h>
+#include <stdlib.h>
+
+//creating structure for node
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+//creating  start pointer of link list
+struct node *start = NULL;
+
+//create a function for creating variable dynamically
+struct node* createNode()
+{
+    struct node *n; //variable of pointer
+
+    n = (struct node*)malloc(sizeof(struct node)); // malloc return address of created block  we have type casting into struct type
+    return(n); //return address of memory block
+
+}
+
+//create a function for inserting data
+void insertNode()
+{
+    struct node *temp,*t;
+    temp = createNode();
+    printf("Enter a number ");
+    scanf("%d",&temp->data);
+    temp->link = NULL;
+
+    //checking start pointer is empty or not
+    if(start == NULL)
+    {
+        start = temp;
+    }
+    //insert node if start has a least one node
+    else
+    {
+        t = start;
+        while(t->link !=NULL)
+        {
+            t = t->link;
+        }
+        t->link= temp;
+        printf("Node Element inserted at last!\n");
+    }
+}
+
+//create a function for deleting node
+void deleteNode()
+{
+    struct node *remove;
+    //checking the link list empty or not
+    if(start == NULL)
+        {
+            printf("List is empty\n");
+        }
+    else
+    {
+        remove = start;  //assigning value of start pointer into temperary remove pointer
+        start = start->link; //assigning value of first node in start pointer
+        free(remove);  //releasing memory of remove pointer
+        printf("First Node element deleted !\n");
+    }
+
+}
+
+//creating function for showing link list
+void showList()
+{
+    struct node *Temp,*t;
+    if(start == NULL)
+        printf("Link list is Empty\n");
+    else
+    {
+        printf("List is \n");
+        t = start;
+
+        while(t!=NULL)
+        {
+            printf(" %d ",t->data);
+            t = t->link;
+        }
+
+    }
+}
+
+int menu()
+{
+    int m;
+    printf("\t Link list insertion deletion traversing \n");
+    printf("1. Add value to the list\n");
+    printf("2. Delete first value\n");
+    printf("3. Show List\n");
+    printf("4. Exit\n");
+    printf("\nEnter ");
+    scanf("%d",&m);
+    return m;
+}
+
+void main()
+{
+    while(1)
+    {
+        system("cls");
+        switch(menu())
+        {
+        case 1:
+            insertNode();
+            break;
+        case 2:
+            deleteNode();
+            break;
+        case 3:
+            showList();
+            break;
+        case 4:
+            exit(0);
+        default:
+            printf("Invalid input!\n");
+
+        }
+        system("pause");
+    }
+    //main function
+
+
+}
